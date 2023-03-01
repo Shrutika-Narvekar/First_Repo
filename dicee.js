fd7e5813e850c5f0bdab2244cf8524becfed8e46
Shrutika N <shrutikanarver05@gmail.com>
2023-03-01 13:00:10 +0530

    second commit with winning

--- dicee.js
+++ dicee.js
@@ -1,8 +1,23 @@
-window.onload = selectPic;
-var myPic = new Array("C:\Users\Dell\Downloads\Dicee Challenge - Starting Files\Dicee Challenge - Starting Files\images\dice1.png", "C:\Users\Dell\Downloads\Dicee Challenge - Starting Files\Dicee Challenge - Starting Files\images\dice2.png", "\images\dice3.png")
 
-function selectPic(){
-    var random = Math.floor(Math.random() * myPic.length);
-    document.getElementById("myPicture").src = myPic[random];
 
-}
+    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+    var diceImage = "dice" + randomNumber1 + ".png";
+    var randomImageSource = "images/" + diceImage;
+    var image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src",randomImageSource);
+
+    var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    var image2 = document.querySelectorAll("img")[1];
+    image2.setAttribute("src",randomImageSource2);
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML="Player 1 wins";
+    }else if (randomNumber1 == randomNumber2){
+
+    document.querySelector("h1").innerHTML = " Draw";
+
+    }else{document.querySelector("h1").innerHTML="Player 2 wins";
+
+        
+    }
